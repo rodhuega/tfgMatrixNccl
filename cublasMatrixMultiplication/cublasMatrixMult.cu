585a3b21eb5ae566209516c563941e2dea6f9c7c
Rodrigo Huerta Gañán <rodhuega@users.noreply.github.com>
2019-10-06 22:40:40 +0200

    cambios cublas multiplicacion simple

--- cublasMatrixMultiplication/cublasMatrixMult.cu
+++ cublasMatrixMultiplication/cublasMatrixMult.cu
@@ -4,20 +4,22 @@
 
 #define N 12
 
-void print_matrix(int nr_rows_A, int nr_cols_A,float* A) {
+using namespace std;
 
-    for(int i = 0; i < nr_rows_A; ++i){
-        for(int j = 0; j < nr_cols_A; ++j){
-            std::cout << A[j * nr_rows_A + i] << " ";
+void MostrarMatriz(int filas, int columnas,float* A) {
+
+    for(int i = 0; i < filas; ++i){
+        for(int j = 0; j < columnas; ++j){
+            cout << A[j * filas + i] << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 int main() {
 	
-	int sizeMalloc=N*N* sizeof(float);
+	int tamMalloc=N*N* sizeof(float);
 	float h_A[N][N]={{147,  67,  56, 124, 151, 111,  89,  24,  39, 141, 151,  52},
 	{  8, 146,  74, 101, 112, 150,   9, 174,  67,   3,  48, 102},
 	{ 87, 146,  86,  17, 198,  98,  43, 143, 155,  26, 160,  62},
@@ -42,20 +44,22 @@ int main() {
 	{177, 125, 179,  36,  79,  90, 195, 161, 119, 165,  29, 120},
 	{154,   2, 151, 164, 174, 118,  25,  82, 110, 112, 138,  11},
 	{152,  78, 111, 132, 176, 181,  34, 133, 155,  14,  88,   9}};
-	float *h_C = (float *)malloc(sizeMalloc);
+	float *h_C = (float *)malloc(tamMalloc);
 
 	float *d_A, *d_B, *d_C;
-	cudaMalloc(&d_A,sizeMalloc);
-	cudaMalloc(&d_B,sizeMalloc);
-	cudaMalloc(&d_C,sizeMalloc);
+	cudaMalloc(&d_A,tamMalloc);
+	cudaMalloc(&d_B,tamMalloc);
+	cudaMalloc(&d_C,tamMalloc);
 
-	cudaMemcpy(d_A,h_A,sizeMalloc,cudaMemcpyHostToDevice);
-	cudaMemcpy(d_B,h_B,sizeMalloc,cudaMemcpyHostToDevice);
+	cudaMemcpy(d_A,h_A,tamMalloc,cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B,h_B,tamMalloc,cudaMemcpyHostToDevice);
 
 	std::cout << "A =" << std::endl;
-	print_matrix(N, N,(float*) h_A);
+	MostrarMatriz
+(N, N,(float*) h_A);
 	std::cout << "B =" << std::endl;
-	print_matrix(N, N,(float*)h_B);
+	MostrarMatriz
+(N, N,(float*)h_B);
 
 	float alfa = 1;
 	float beta = 0;
@@ -67,9 +71,10 @@ int main() {
 
 	cublasDestroy(handle);
 
-	cudaMemcpy(h_C,d_C,sizeMalloc,cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_C,d_C,tamMalloc,cudaMemcpyDeviceToHost);
 	std::cout << "C =" << std::endl;
-	print_matrix( N, N,h_C);
+	MostrarMatriz
+( N, N,h_C);
 	cudaFree(d_A);
 	cudaFree(d_B);
 	cudaFree(d_C);
