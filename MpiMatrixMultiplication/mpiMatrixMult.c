514935e025f278ebb39ac05a96813fcb4bc1b332
Rodrigo Huerta Gañán <rodhuega@users.noreply.github.com>
2020-02-08 13:22:17 +0100

    arreglado multiplicacion de matrices basico

--- MpiMatrixMultiplication/mpiMatrixMult.c
+++ MpiMatrixMultiplication/mpiMatrixMult.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <mpi.h>
 
-#define blockRowLength 2
-#define N blockRowLength *blockRowLength
+#define blockNSize 2
+#define N blockNSize *blockNSize
 
 void MostrarMatriz(int filas, int columnas, double *A)
 {
@@ -23,17 +23,17 @@ void MostrarMatriz(int filas, int columnas, double *A)
 void RealizarMultiplicacion(int cpuRank, double *A, double *B, double *C)
 {
     int i, j, k, sum;
-    for (i = 0; i < blockRowLength; i++)
+    for (i = 0; i < blockNSize; i++)
     {
         sum = 0;
-        for (j = 0; j < blockRowLength; j++)
+        for (j = 0; j < blockNSize; j++)
         {
             sum = 0;
-            for (k = 0; k < blockRowLength; k++)
+            for (k = 0; k < blockNSize; k++)
             {
-                sum += A[i * blockRowLength + j] * B[i + blockRowLength * j];
+                sum += A[i * blockNSize + j] * B[i + blockNSize * j];
             }
-            C[i * blockRowLength + j] = sum;
+            C[i * blockNSize + j] = sum;
         }
     }
 }
@@ -41,23 +41,23 @@ void RealizarMultiplicacion(int cpuRank, double *A, double *B, double *C)
 void copiarMiniMatriz(int primeraFila,int primeraColumna,double *A,double* a_local)
 {
     int i;
-    for(i=0;i<blockRowLength;i++)
+    for(i=0;i<blockNSize;i++)
     {
-        memcpy(a_local+i*blockRowLength, A+N*primeraFila+primeraColumna, sizeof(double)*blockRowLength);
+        memcpy(a_local+i*blockNSize, A+N*primeraFila+primeraColumna, sizeof(double)*blockNSize);
         primeraFila+=1;
     }
 }
 
-void RealizarMultiplicacionBloque(int cpuRank, int blockSize, double *A, double *B, double *c_local)
+void RealizarMultiplicacionBloque(int cpuRank, int blockNumberOfElements, double *A, double *B, double *c_local)
 {
     int i, j;
     //Realizamos las dos multiplicaciones necesarias
-    double a1_local[blockRowLength][blockRowLength];
-    double a2_local[blockRowLength][blockRowLength];
-    double b1_local[blockRowLength][blockRowLength];
-    double b2_local[blockRowLength][blockRowLength];
-    double c1_local[blockRowLength][blockRowLength];
-    double c2_local[blockRowLength][blockRowLength];
+    double a1_local[blockNSize][blockNSize];
+    double a2_local[blockNSize][blockNSize];
+    double b1_local[blockNSize][blockNSize];
+    double b2_local[blockNSize][blockNSize];
+    double c1_local[blockNSize][blockNSize];
+    double c2_local[blockNSize][blockNSize];
     if (cpuRank == 0)
     {
         copiarMiniMatriz(0,0,A,a1_local);
@@ -89,18 +89,18 @@ void RealizarMultiplicacionBloque(int cpuRank, int blockSize, double *A, double 
     RealizarMultiplicacion(cpuRank, a1_local, b1_local, c1_local);
     RealizarMultiplicacion(cpuRank, a2_local, b2_local, c2_local);
     //Sumamos los productos de esas multiplicaciones
-    for (i = 0; i < blockRowLength; i++)
+    for (i = 0; i < blockNSize; i++)
     {
-        for (j = 0; j < blockRowLength; j++)
+        for (j = 0; j < blockNSize; j++)
         {
-            c_local[blockRowLength * i + j] = c1_local[i][j] + c2_local[i][j];
+            c_local[blockNSize * i + j] = c1_local[i][j] + c2_local[i][j];
         }
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int cpuRank, cpuSize, blockSize;
+    int cpuRank, cpuSize, blockNumberOfElements;
     // double a[N][N] = {{147, 67, 56, 124, 151, 111, 89, 24, 39, 141, 151, 52},
     //                   {8, 146, 74, 101, 112, 150, 9, 174, 67, 3, 48, 102},
     //                   {87, 146, 86, 17, 198, 98, 43, 143, 155, 26, 160, 62},
@@ -134,53 +134,38 @@ int main(int argc, char *argv[])
                       {7, 7, 8, 8},
                       {7, 7, 8, 8}};
     double c[N][N];
-    double c_local[blockRowLength][blockRowLength];
+    double c_local[blockNSize][blockNSize];
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &cpuSize);
     MPI_Comm_rank(MPI_COMM_WORLD, &cpuRank);
     MPI_Datatype matrixLocalType;
-    blockSize = N * N / cpuSize;
+    blockNumberOfElements = N * N / cpuSize;
+    int send_counts[4] = {1, 1, 1, 1};
+    int blocks[4] = {
+        0, blockNSize,
+        N*blockNSize, N*blockNSize + blockNSize
+    };
 
-    //Idea que puede funcionar para nccl, broadcast de a y b, ya que no se puede hacer scatter
-    //En a y en b despues en cada nodo coger los bloques que interese.
-    //Otra idea pasar a y b con scatter por mpi y luego copiarlo al device
-    RealizarMultiplicacionBloque(cpuRank,blockSize,a,b,c_local);
-    // MostrarMatriz(blockRowLength, blockRowLength, (double *)c_local);
-    int *displs = (int *)malloc(blockRowLength*sizeof(double)); 
-    int *rcounts = (int *)malloc(blockRowLength*sizeof(int)); 
-    int i=0;
-    for (i=0; i<blockRowLength; i++) { 
-        displs[i] = i*blockRowLength; 
-        rcounts[i] = blockRowLength; 
-    } 
-    // MPI_Type_vector(cpuSize,blockRowLength,N,MPI_DOUBLE,&matrixLocalType);
-    // MPI_Type_commit(&matrixLocalType);
-    // MPI_Gather(c_local, blockSize, MPI_DOUBLE, c, blockSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    // MPI_Gatherv(c_local,1,matrixLocalType,c,1,blockRowLength,MPI_DOUBLE,0,MPI_COMM_WORLD);
-    // MPI_Gatherv(c_local,1,matrixLocalType,c,rcounts,displs,MPI_DOUBLE,0,MPI_COMM_WORLD);
-    // MPI_Gather(c_local, 1, matrixLocalType, c, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    int salto=0;
-    c[2][1]=1;
-    if(cpuRank>1)
-    {
-        salto=N*blockRowLength;
-        printf("El valor de salto es: %f", *(c+salto));
+    if (cpuRank == 0) {
+        int sizes[2] = {N, N};
+        int subsizes[2] = {blockNSize, blockNSize};
+        int starts[2] = {0, 0};
+        MPI_Type_create_subarray(2, sizes, subsizes, starts,
+            MPI_ORDER_C, MPI_DOUBLE, &matrixLocalType);
+
+        int double_size;
+        MPI_Type_size(MPI_DOUBLE, &double_size);
+        MPI_Type_create_resized(matrixLocalType, 0, 1*double_size, &matrixLocalType);
+        MPI_Type_commit(&matrixLocalType);
     }
-    for(i=0;i<1;i++)
-    {
 
-        MPI_Gather(c_local[i], blockRowLength, MPI_DOUBLE, c+salto+2, blockRowLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-        // if(cpuRank<2)
-        // {
-        // }else 
-        // {
-        //     MPI_Gather(c_local[i], blockRowLength, MPI_DOUBLE, c+N*i, blockRowLength, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-        // }
-    }
+    RealizarMultiplicacionBloque(cpuRank,blockNumberOfElements,a,b,c_local);
+ 
+    MPI_Gatherv(c_local, blockNumberOfElements, MPI_DOUBLE, c, send_counts, blocks, matrixLocalType, 0, MPI_COMM_WORLD);
     if(cpuRank==0)
     {
+        MPI_Type_free(&matrixLocalType);
         MostrarMatriz(N,N,(double*)c);
     }
     MPI_Finalize();
