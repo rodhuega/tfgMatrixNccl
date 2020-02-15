c60d76266826629bf48fe6517b9cd90090008a50
Rodrigo Huerta <rodhuega@inf.upv.es>
2020-02-15 20:46:41 +0100

    Eliminado codigo inutil del main

--- MpiMatrixMultiplicationCpp/src/main.cpp
+++ MpiMatrixMultiplicationCpp/src/main.cpp
@@ -12,67 +12,6 @@
 
 using namespace std;
 
-
-
-// void copiarMiniMatriz(int primeraFila,int primeraColumna,double **A,double* a_local)
-// {
-//     int i;
-//     for(i=0;i<blockRowSize;i++)
-//     {
-//         memcpy(a_local+i*blockRowSize, &A[primeraFila][primeraColumna], sizeof(double)*blockRowSize);
-//         primeraFila+=1;
-//     }
-// }
-
-// void RealizarMultiplicacionBloque(int cpuRank, int blockNumberOfElements, double **A, double **B, double *c_local)
-// {
-//     int i, j;
-//     //Realizamos las dos multiplicaciones necesarias
-//     double a1_local[blockRowSize][blockRowSize];
-//     double a2_local[blockRowSize][blockRowSize];
-//     double b1_local[blockRowSize][blockRowSize];
-//     double b2_local[blockRowSize][blockRowSize];
-//     double c1_local[blockRowSize][blockRowSize];
-//     double c2_local[blockRowSize][blockRowSize];
-//     if (cpuRank == 0)
-//     {
-//         copiarMiniMatriz(0,0,A,a1_local);
-//         copiarMiniMatriz(0,blockRowSize,A,a2_local);
-//         copiarMiniMatriz(0,0,B,b1_local);
-//         copiarMiniMatriz(blockRowSize,0,B,b2_local);
-//     }
-//     if (cpuRank == 1)
-//     {
-//         copiarMiniMatriz(0,0,A,a1_local);
-//         copiarMiniMatriz(0,blockRowSize,A,a2_local);
-//         copiarMiniMatriz(0,blockRowSize,B,b1_local);
-//         copiarMiniMatriz(blockRowSize,blockRowSize,B,b2_local);
-//     }
-//     if (cpuRank == 2)
-//     {
-//         copiarMiniMatriz(blockRowSize,0,A,a1_local);
-//         copiarMiniMatriz(blockRowSize,blockRowSize,A,a2_local);
-//         copiarMiniMatriz(0,0,B,b1_local);
-//         copiarMiniMatriz(blockRowSize,0,B,b2_local);
-//     }
-//     if (cpuRank == 3)
-//     {
-//         copiarMiniMatriz(blockRowSize,0,A,a1_local);
-//         copiarMiniMatriz(blockRowSize,blockRowSize,A,a2_local);
-//         copiarMiniMatriz(0,blockRowSize,B,b1_local);
-//         copiarMiniMatriz(blockRowSize,blockRowSize,B,b2_local);
-//     }
-//     RealizarMultiplicacion(cpuRank, a1_local, b1_local, c1_local);
-//     RealizarMultiplicacion(cpuRank, a2_local, b2_local, c2_local);
-//     //Sumamos los productos de esas multiplicaciones
-//     for (i = 0; i < blockRowSize; i++)
-//     {
-//         for (j = 0; j < blockRowSize; j++)
-//         {
-//             c_local[blockRowSize * i + j] = c1_local[i][j] + c2_local[i][j];
-//         }
-//     }
-
 int main(int argc, char *argv[])
 {
     int cpuRank, cpuSize;
