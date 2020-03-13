f81fc17e79f9c6c2ea96a1aca181a7907a02bfe9
rodhuega <rodhuega@inf.upv.es>
2020-03-13 20:50:04 +0100

    Avanzado en medidor de tiempos sin mucha utiliad. No acabado. Puede que cublasXt haga lo mismo

--- ncclPruebas/ncclComunicadores.cu
+++ ncclPruebas/ncclComunicadores.cu
@@ -6,9 +6,12 @@
 #include <iomanip>
 #include <algorithm>
 #include "cuda_runtime.h"
+#include <cublas_v2.h>
 #include "nccl.h"
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
+
+#define IDX2C(i,j,ld) (((j)*(ld))+(i))
 
 #define CUDACHECK(cmd)                                         \
 	do                                                         \
@@ -34,7 +37,78 @@
 		}                                                      \
 	} while (0)
 
+#define cudaCalloc(A, B, C) \
+    do { \
+        cudaError_t __cudaCalloc_err = cudaMalloc(A, B*C); \
+        if (__cudaCalloc_err == cudaSuccess) CUDACHECK(cudaMemset(*A, 0, B*C)); \
+    } while (0)
+
 using namespace std;
+
+
+struct OperationProperties
+{
+    /**
+     * @brief Tamaño de las filas de la malla
+     * 
+     */
+    int meshRowSize;
+    /**
+     * @brief Tamaño de las columnas de la malla
+     * 
+     */
+    int meshColumnSize;
+    /**
+     * @brief Filas operacionales que tendra la matriz A
+     * 
+     */
+    int rowsA;
+    /**
+     * @brief Columnas de la Matriz A o Filas de la Matriz B operacionales
+     * 
+     */
+    int columnsAorRowsB;
+    /**
+     * @brief Columnas operacionales que tendra la matriz B
+     * 
+     */
+    int columnsB;
+    /**
+     * @brief Numeros de 0s que tendra la matriz operacional al extenderse
+     * 
+     */
+    int numberOf0;
+    /**
+     * @brief Numero de procesadores que realizaran la operacion de multiplicacion
+     * 
+     */
+    int cpuSize;
+    /**
+     * @brief Numero de filas que tendra la matriz A de forma local
+     * 
+     */
+    int blockRowSizeA;
+    /**
+     * @brief Numero de columnas que tendra la matriz A de forma local
+     * 
+     */
+    int blockColumnSizeA;
+    /**
+     * @brief Numero de filas que tendra la matriz B de forma local
+     * 
+     */
+    int blockRowSizeB;
+    /**
+     * @brief Numero de columnas que tendra la matriz B de forma local
+     * 
+     */
+    int blockColumnSizeB;
+    /**
+     * @brief Incida si las propiedades antes indicadas son aptas para el calculo de la multiplicaicon de la matriz
+     * 
+     */
+    bool candidate;
+};
 
 struct GpuProperties
 {
@@ -53,6 +127,9 @@ struct GpuProperties
 	ncclComm_t *commRow;
 	ncclComm_t *commCol;
 	cudaStream_t *stream;
+	double *matrixDevice;
+	int rowSize;
+	int colSize;
 
 	GpuProperties(int devicesTotal, int rankGlobal)
 	{
@@ -60,6 +137,7 @@ struct GpuProperties
 		this->rankGlobal = rankGlobal;
 	}
 };
+
 
 __global__ void
 cudaHelloWorld()
@@ -79,7 +157,7 @@ void printMatrix(int rows, int columns, double *M)
     {
         for (j = 0; j < columns; ++j)
         {
-            matrixIndex = matrixCalculateIndex(columns, i, j);
+            matrixIndex = IDX2C(i, j,rows);
             cout << M[matrixIndex] << "\t";
         }
         cout << endl;
@@ -120,7 +198,7 @@ double *ReadOrGenerateRandomMatrix(bool isRandom, const char *fileName, int &row
 	{
 		for (j = 0; j < columns; j++)
 		{
-			matrixIndex = matrixCalculateIndex(columns, i, j);
+			matrixIndex = IDX2C(i,j,rows);
 			if (isRandom)
 			{
 				matrix[matrixIndex] = distr(eng);
@@ -137,6 +215,73 @@ double *ReadOrGenerateRandomMatrix(bool isRandom, const char *fileName, int &row
 	}
 	return matrix;
 }
+
+OperationProperties calculateNonEqualMesh(int rowsA, int columnsAorRowsB, int columnsB, int nCpusMesh1, int nCpusMesh2, bool isMeshRow)
+{
+    OperationProperties res;
+    if (isMeshRow)
+    {
+        res.meshRowSize = nCpusMesh1;
+        res.meshColumnSize = nCpusMesh2;
+    }
+    else
+    {
+        res.meshColumnSize = nCpusMesh1;
+        res.meshRowSize = nCpusMesh2;
+    }
+    res.cpuSize = res.meshRowSize * res.meshColumnSize;
+    res.rowsA = ceil(rowsA / (float)res.meshRowSize) * res.meshRowSize;
+    res.columnsAorRowsB = ceil(columnsAorRowsB / (float)res.meshColumnSize) * res.meshColumnSize;
+    res.columnsB = ceil(columnsB / (float)res.meshColumnSize) * res.meshColumnSize;
+    int numberOf0atA = (res.rowsA * res.columnsAorRowsB) - (rowsA * columnsAorRowsB);
+    int numberOf0atB = (res.columnsB * res.columnsAorRowsB) - (columnsAorRowsB * columnsB);
+    res.numberOf0 = numberOf0atA + numberOf0atB;
+    //PUEDE QUE AQUI NECESITE UN IF DEPENDIENDO DE CUAL SEA EL GRID DOMINANTE; DE MOMENTO EL GRID DOMINANTE AHORA ES A SIEMPRE
+    res.blockColumnSizeA = res.columnsAorRowsB / res.meshColumnSize;
+    res.blockRowSizeB = res.blockColumnSizeA;
+    res.blockRowSizeA = res.rowsA / res.meshRowSize;
+    res.blockColumnSizeB = res.columnsB / res.meshColumnSize;
+    res.candidate = res.meshColumnSize > 1 && res.meshRowSize > 1;
+    return res;
+}
+
+OperationProperties getMeshAndMatrixSize(int rowsA, int columnsA, int rowsB, int columnsB, int cpuSize)
+{
+    OperationProperties res;
+
+    //Se calculan todas las posibilidadades y se selecciona la que mas cpus use y menos 0 contenga de esas opciones, Solo se añaden elementos validos(ninguno con meshDimension 1 o 0)
+    int i, j, numberOfZerosA, numberOfZerosB;
+    vector<OperationProperties> allOp;
+    vector<OperationProperties> sameCpuSizeOp;
+    for (i = 2; i < cpuSize - 1; i++)
+    {
+        for (j = i; j * i <= cpuSize; j++)
+        {
+            OperationProperties opRow = calculateNonEqualMesh(rowsA, rowsB, columnsB, i, j, true);
+            OperationProperties opColumn = calculateNonEqualMesh(rowsA, rowsB, columnsB, i, j, false);
+            if (opRow.candidate)
+            {
+                allOp.push_back(opRow);
+            }
+            if (opColumn.candidate)
+            {
+                allOp.push_back(opColumn);
+            }
+        }
+    }
+    sort(begin(allOp), end(allOp), [](OperationProperties op1, OperationProperties op2) {
+        if (op1.cpuSize != op2.cpuSize)
+        {
+            return op1.cpuSize > op2.cpuSize;
+        }
+        return op1.numberOf0 < op2.numberOf0;
+    });
+    res = allOp[0];
+
+    return res;
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -184,17 +329,22 @@ int main(int argc, char *argv[])
 		matrixA = ReadOrGenerateRandomMatrix(true, "", rowsA, columnsA, atoi(optionsCmd[rPosition + 3].c_str()), atoi(optionsCmd[rPosition + 4].c_str()));
 	}
 	printMatrix(rowsA,columnsA,matrixA);
-	//Configuracion del comunicador que tiene a todos los dispositivos
 	int nDevicesGlobal;
-	max(cudaGetDeviceCount(&nDevicesGlobal), 4);
+	CUDACHECK(cudaGetDeviceCount(&nDevicesGlobal));
+	OperationProperties op = getMeshAndMatrixSize(rowsA, columnsA, rowsA, columnsA, nDevicesGlobal);
+	//Configuracion del comunicador que tiene a todos los dispositivos
+	
+	
 	GpuProperties *gpusInfo = (GpuProperties *)malloc(sizeof(GpuProperties) * nDevicesGlobal);
 
 	int devicesGlobal[nDevicesGlobal];
 	for (int i = 0; i < nDevicesGlobal; i++)
 	{
+		CUDACHECK(cudaSetDevice(i));
 		gpusInfo[i] = GpuProperties(nDevicesGlobal, i);
 		gpusInfo[i].stream = (cudaStream_t *)malloc(sizeof(cudaStream_t));
 		devicesGlobal[i] = i;
+		CUDACHECK(cudaMalloc ((void**)&gpusInfo[i].matrixDevice, op.blockRowSizeA*op.blockColumnSizeA*sizeof(double)));
 	}
 
 	ncclComm_t commGlobal[nDevicesGlobal];
