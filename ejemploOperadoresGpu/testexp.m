bd6f547bdf727b58e6b13bc1e4f2dc0da88bfc28
rodhuega <rodhuega@inf.upv.es>
2020-03-29 18:00:05 +0200

    Agregado al repositiorio el ejemplo del profesor que muestra como sobrecargar operadores

---
+++ ejemploOperadoresGpu/testexp.m
@@ -0,0 +1,38 @@
+function [t_cpu_tay,t_gpu_tay,t_cpu_ber,t_gpu_ber] = testexp(n_ini,inc,n_end);
+% [t_cpu_tay,t_gpu_tay,t_cpu_ber,t_gpu_ber] = testexp(n_ini,inc,n_end);
+%
+% Funcion utilizada para la comparativa del artículo JCAM del CMMSE2019
+
+% Dummy entries to load GPU driver
+A=rand(100);
+[fA,m,s,nProd] = fun_pol('exp','bernoulli','conEstNorma','sinGPUs',A);
+[fB,m,s,nProd] = fun_pol('exp','bernoulli','conEstNorma','conGPUs',A);
+
+ind_t = 1;
+for i=n_ini:inc:n_end
+  A=rand(i);
+  A=A/norm(A);%*500;
+
+  tic
+  [fA,m,s,np] = fun_pol('exp','taylor','conEstNorma','sinGPUs',A);
+  t = toc;
+  t_cpu_tay(ind_t) = t;
+
+  tic
+  [fA,m,s,np] = fun_pol('exp','bernoulli','conEstNorma','sinGPUs',A);
+  t = toc;
+  t_cpu_ber(ind_t) = t;
+
+  tic
+  [fB,m,s,np] = fun_pol('exp','taylor','conEstNorma','conGPUs',A);
+  t = toc;
+  t_gpu_tay(ind_t) = t;
+
+  tic
+  [fB,m,s,np] = fun_pol('exp','bernoulli','conEstNorma','conGPUs',A);
+  t = toc;
+  t_gpu_ber(ind_t) = t;
+
+  ind_t=ind_t+1;
+  disp(norm(fA-fB)/norm(fA))
+end
